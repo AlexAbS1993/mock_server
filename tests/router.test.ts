42b7c890235aaf9e49d1eabf77a4777a85825a37
Alex Abetov <abetovalex93@gmail.com>
2025-01-20 04:36:06 +0300

    Реализована первая версия Router
    1) Установлена прямая зависимость от Route
    2) Проведены некоторые тесты, связанные с working_dictionary внутри
       Router
    3) Необходима работа с типом handler
    4) Необходимо понять, как в Handler будут приходить данные

--- tests/router.test.ts
+++ tests/router.test.ts
@@ -1,10 +1,23 @@
-const Router = require("../router/router")
+import Route from "../router/routes"
+import { Router } from "../router/router"
+import { url_parser_to_array } from "../router/helpers/url_parser"
 
 describe('Router принимает в себя роуты с обработчиками и выдаёт список роутов, проходя по списку зарегистрированных', () => {
     let router = new Router()
+    const mock_path = '/test/feature'
+    test('url_parser работает корректно', () => {
+        let parsedPath = url_parser_to_array(mock_path)
+        expect(parsedPath[0]).toBe('test')
+        expect(parsedPath[1]).toBe('feature')
+        let anotherParsedPath = url_parser_to_array('/hello/with/options?hey=10')
+        expect(anotherParsedPath[2]).toBe('options')
+    })
     describe('Router может сохранять в себе объекты роутов', () => {
-        test('Роуты должны соответствовать требуемому интерфейсу', () => {
-            expect(router).toBeDefined()
+        test('Когда роутер определяет роут, то в его route_dictionary появляется путь к роуту', () => {
+            router.defineRouteFor(mock_path)
+            expect(router.routes_dictionary.test).toBeDefined()
+            expect(router.routes_dictionary.test.feature).toBeDefined()
+            expect(router.routes_dictionary.test.feature.route instanceof Route).toBe(true)
         })
     })
 })
