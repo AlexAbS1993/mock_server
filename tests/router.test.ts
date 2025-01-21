e8a05ddd1746cc1dff0f207cd7903e620a6134e4
Alex Abetov <abetovalex93@gmail.com>
2025-01-22 01:12:59 +0300

    Функционал сущности Router расширен в соответствии с требованиями
    1) Теперь Router может передавать обработку данных хэндлеру по
       соответствующему пути и методу;
       2) Метод deligateHandle принимает путь, метод и данные (их необходимо
          собрать для хэндлера заранее);
          3) Дополнены и проведены тесты Router;
          4) Изменено название метода isAvailable на isPathExists
    
          minor:
          Сущность Route теперь экспортируется из index.ts

--- tests/router.test.ts
+++ tests/router.test.ts
@@ -1,6 +1,7 @@
 import { Route } from "../router/routes"
 import { Router } from "../router/router"
 import { url_parser_to_array } from "../router/helpers/url_parser"
+import { METHODS } from "../router/helpers/methods"
 
 describe('Router принимает в себя роуты с обработчиками и выдаёт список роутов, проходя по списку зарегистрированных', () => {
     let router = new Router()
@@ -22,17 +23,36 @@ describe('Router принимает в себя роуты с обработчи
             expect(router.routes_dictionary.test.feature).toBeDefined()
             expect(router.routes_dictionary.test.feature.route instanceof Route).toBe(true)
         })
-        test('Router после определения роута может добавлять на него обработчики методов', () => {
+        test('Router после определения роута может добавлять на него обработчики методов и вызвать их', async () => {
             router
                 .defineRouteFor(mock_path)
-                .get({ execute() { } })
+                .get({ execute() { return 1 } })
                 .put({ execute() { } })
                 .defineRouteFor(`${mock_path}/nextstep`)
+                .post({ execute(data: any) { return data.log } })
+                .get({ execute() { } })
             expect(router.routes_dictionary.test).toBeDefined()
             expect(router.routes_dictionary.test.feature).toBeDefined()
             expect(router.routes_dictionary.test.feature.nextstep).toBeDefined()
             expect(router.routes_dictionary.test.feature.route instanceof Route).toBe(true)
             expect(router.routes_dictionary.test.feature.nextstep.route instanceof Route).toBe(true)
+            expect(await router.deligateHandle(mock_path, METHODS.GET)).toBeDefined()
+            expect(await router.deligateHandle(mock_path, METHODS.GET)).toBe(1)
+            expect(await router.deligateHandle(`${mock_path}/nextstep`, METHODS.POST, { log: 'log' })).toBe('log')
+            try {
+                await router.deligateHandle(mock_path, METHODS.POST)
+            }
+            catch (e: any) {
+                expect(e instanceof Error).toBeTruthy()
+                expect(e.message).toBe(`Путь ${mock_path} с методом ${METHODS.POST} не существует`)
+            }
+            try {
+                await router.deligateHandle('/status', METHODS.POST)
+            }
+            catch (e: any) {
+                expect(e instanceof Error).toBeTruthy()
+                expect(e.message).toBe(`Путь /status с методом ${METHODS.POST} не существует`)
+            }
         })
     })
 })
